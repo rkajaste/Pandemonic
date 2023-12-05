e7399dc9a14c8303478c5d0427716b63b5aaf58e
Richard Kajaste <rkajaste@users.noreply.github.com>
2023-12-05 03:51:11 +0200

    add imgui support

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #define STB_IMAGE_IMPLEMENTATION
 
 #include <glad/glad.h>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -34,6 +39,19 @@ int main()
     glfwSwapInterval(0);
     gladLoadGL();
     fprintf(stderr, "OpenGL Version %s\n", glGetString(GL_VERSION));
+    
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
+
 
     glfwSetKeyCallback(window, key_callback);
 
@@ -57,6 +75,8 @@ int main()
         GLfloat frameTime = newFrameTime - lastFrameTime;
         if (frameTime >= maxPeriod)
         {
+            glfwPollEvents();
+        
             if (frameTime > 0.25f)
             {
                 frameTime = 0.25f;
@@ -71,7 +91,13 @@ int main()
                 game->update(dt);
                 accumulator -= dt;
             }
-
+            
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+            ImGui::ShowDemoWindow(); // Show demo window! :)
+            
             glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glClearDepth(1.0f);
@@ -82,16 +108,24 @@ int main()
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
             game->render();
-
+            
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            
             glDisable(GL_BLEND);
             glfwSwapBuffers(window);
-            glfwPollEvents();
+            
         }
     }
 
     // Delete all resources as loaded using the resource manager
     ResourceManager::Clear();
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
